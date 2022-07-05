32976604d09ef35bad68024fc14d62b7fd007588
ViniciusPosssatto <101053966+ViniciusPosssatto@users.noreply.github.com>
2022-07-05 14:29:26 -0300

    Update infos.py
    Adicionado uma base de dados para ser usado em testes nas classes

--- app/data/infos.py
+++ app/data/infos.py
@@ -1,5 +1,19 @@
-motos = []
 
-camionete = []
+motosTriciclos = [{'potencia': 150, 'rodas': 2, 'chassi': 1234124123, 'data-fabricação': '12-11-2020',
+                  'modelo': 'cg titan', 'placa': 'dpt1j23', 'valor': 10300, 'cpf': 0, 'cor': 'preta'}]
 
-carro = []
+carros = [
+    {'potencia': 97, 'portas': 4, 'combustivel': 'gasolina', 'chassi': 1234123333, 'data-fabricação': '5-10-2020',
+     'modelo': 'gol', 'placa': 'ard5i34', 'valor': 50300, 'cpf': 0, 'cor': 'vermelho'},
+    {'potencia': 89, 'portas': 4, 'combustivel': 'gasolina', 'chassi': 1234123345, 'data-fabricação': '11-1-2020',
+     'modelo': 'palio', 'placa': 'dtt1i23', 'valor': 35800, 'cpf': 0, 'cor': 'azul'}
+]
+
+camionetes = [
+    {'potencia': 355, 'portas': 2, 'cacamba': 450, 'combustivel': 'gasolina', 'chassi': 1234123213, 'data-fabricação':
+     '12-11-2020', 'modelo': 's10 simples', 'placa': 'dre1s23', 'valor': 110300, 'cpf': 0, 'cor': 'roxa'},
+    {'potencia': 400, 'portas': 4, 'cacamba': 344, 'combustivel': 'diesel', 'chassi': 1234124545,
+     'data-fabricação': '10-9-2020', 'modelo': 'hilux', 'placa': 'dte1k23', 'valor': 130300, 'cpf': 0, 'cor': 'roxa'},
+    {'potencia': 332, 'portas': 6, 'cacamba': 200, 'combustivel': 'diesel', 'chassi': 12341246772,
+     'data-fabricação': '13-8-2020', 'modelo': 'amarok', 'placa': 'ast1u23', 'valor': 150300, 'cpf': 0, 'cor': 'roxa'}
+    ]
