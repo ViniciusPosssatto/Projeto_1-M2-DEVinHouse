97955d3e194f715c3a05ba26a65676fff82fca7c
ViniciusPosssatto <101053966+ViniciusPosssatto@users.noreply.github.com>
2022-07-06 10:59:56 -0300

    Alterado metodo de listar infos de veiculo

--- app/entity/class_veiculo.py
+++ app/entity/class_veiculo.py
@@ -31,22 +31,27 @@ class Veiculo:
             else:
                 print('Chassi não corresponde a um veiculo.')
 
-    @staticmethod
-    def listar_info_veiculo(opcao, chassi):
-        if opcao == 'carro':
-            for car in carros:
-                if chassi == car['chassi']:
-                    print(f"O modelo é {car['modelo']}, possui {car['portas']} portas e {car['potencia']} "
-                          f"cavalos de potência. A placa é {car['placa'].upper()} e o valor: R$ {car['valor']}. "
-                          f"Foi fabricado em {car['data-fabricação']} na cor {car['cor']}.")
-        if opcao == 'moto':
-            for moto in motosTriciclos:
-                print(moto)
-        if opcao == 'camionete':
-            for camionete in camionetes:
-                print(camionete)
-        else:
-            print('Opção inválida.')
+    def listar_info_veiculo(, self, opcao, chassi):
+        if opcao not in self.tipos_veiculos:
+            print('Opção de veículo inválida.')
+        for item in self.tipos_veiculos.get(opcao):
+            if chassi == item['chassi']:
+                if opcao == 'carro':
+                    print(f"O modelo é {item['modelo']}, possui {item['portas']} portas e {item['potencia']} "
+                          f"cavalos de potência. A placa é {item['placa'].upper()} e o valor: R$ {item['valor']}. "
+                          f"Foi fabricado em {item['data-fabricação']} na cor {item['cor']}. O número do chassi é "
+                          f"{item['chassi']}.")
+                elif opcao == 'moto':
+                    print(f"O modelo é {item['modelo']}, tem {item['potencia']} cavalos de potência. A placa é "
+                          f"{item['placa'].upper()} e o valor: R$ {item['valor']}. O número do chassi é "
+                          f"{item['chassi']}. Foi fabricado em {item['data-fabricação']} na cor {item['cor']}.")
+                elif opcao == 'camionete':
+                    print(f"O modelo é {item['modelo']}, possui {item['portas']} portas e {item['potencia']} "
+                          f"cavalos de potência. A placa é {item['placa'].upper()} e o valor: R$ {item['valor']}. "
+                          f"Foi fabricado em {item['data-fabricação']} na cor {item['cor']}. O tamanho da caçamba é de "
+                          f"{item['cacamba']} litros e a numeração do chassi é {item['chassi']}.")
+            else:
+                print('Veículo não encontrado.')
 
     @staticmethod
     def alterar_info_veiculo(opcao, chassi, type_info, nova_info):
