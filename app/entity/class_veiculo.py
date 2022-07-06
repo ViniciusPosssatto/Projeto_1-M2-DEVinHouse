9b6307abd6c675df38915e2b99dc9cb897706b00
ViniciusPosssatto <101053966+ViniciusPosssatto@users.noreply.github.com>
2022-07-06 15:32:35 -0300

    alterado variáveis do init da classe veiculo e metodos
    Alterado variáveis do init para global para que os métodos possam ser estáticos e ai poderem ser chamados no menu sem a necessidade de instanciar a classe veículo.

--- app/entity/class_veiculo.py
+++ app/entity/class_veiculo.py
@@ -4,6 +4,10 @@ from uuid import uuid4
 from data.infos import carros, camionetes, motosTriciclos, historico_vendas
 
 
+data_atual = date.today().strftime("%d/%m/%y")
+tipos_veiculos = {'carro': carros, 'moto': motosTriciclos, 'camionete': camionetes}
+
+
 class Veiculo:
 
     def __init__(self):
@@ -14,8 +18,6 @@ class Veiculo:
         self.valor = None
         self.cpf = 0
         self.cor = None
-        self.data_atual = date.today().strftime("%d/%m/%y")
-        self.tipos_veiculos = {'carro': carros, 'moto': motosTriciclos, 'camionete': camionetes}
 
     def cadastrar_veiculo(self):
         self.modelo = input('Digite o modelo do veiculo: ')
@@ -24,25 +26,27 @@ class Veiculo:
         self.cor = input('Digite a cor predominante no veículo: ')
         self.data_fabr = input('Digite a data de fabricação [dd/mm/aa]: ')
 
-    def vender_veiculo(self, opcao, chassi, cpf, valor):
-        if opcao not in self.tipos_veiculos:
+    @staticmethod
+    def vender_veiculo(opcao, chassi, cpf, valor):
+        if opcao not in tipos_veiculos:
             print('Opção de veículo inválida.')
         veiculo = None
-        for item in self.tipos_veiculos.get(opcao):
+        for item in tipos_veiculos.get(opcao):
             if chassi == item['chassi']:
                 item['cpf'] = cpf
                 print(f"O veiculo modelo {item['modelo']} cuja placa é {item['placa'].upper()}, foi vendido no valor de"
-                      f" R$ {item['valor']} no dia {self.data_atual}.")
+                      f" R$ {item['valor']} no dia {data_atual}.")
                 veiculo = item
-                Historico().save_transation(veiculo, cpf, valor, self.data_atual, opcao)
-                self.tipos_veiculos.get(opcao).remove(item)
+                Historico().save_transation(veiculo, cpf, valor, data_atual, opcao)
+                tipos_veiculos.get(opcao).remove(item)
             else:
                 print('Chassi não corresponde a um veiculo.')
 
-    def listar_info_veiculo(self, opcao, chassi):
-        if opcao not in self.tipos_veiculos:
+    @staticmethod
+    def listar_info_veiculo(opcao, chassi):
+        if opcao not in tipos_veiculos:
             print('Opção de veículo inválida.')
-        for item in self.tipos_veiculos.get(opcao):
+        for item in tipos_veiculos.get(opcao):
             if chassi == item['chassi']:
                 if opcao == 'carro':
                     print(f"O modelo é {item['modelo']}, possui {item['portas']} portas e {item['potencia']} "
@@ -61,10 +65,11 @@ class Veiculo:
             else:
                 print('Veículo não encontrado.')
 
-    def alterar_info_veiculo(self, opcao, chassi, type_info, nova_info):
-        if opcao not in self.tipos_veiculos:
+    @staticmethod
+    def alterar_info_veiculo(opcao, chassi, type_info, nova_info):
+        if opcao not in tipos_veiculos:
             print('Opção de veículo inválida.')
-        for item in self.tipos_veiculos.get(opcao):
+        for item in tipos_veiculos.get(opcao):
             if chassi == item['chassi']:
                 if type_info == 'cor':
                     item['cor'] = nova_info
