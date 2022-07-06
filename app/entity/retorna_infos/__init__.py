469f71f732ec9119e12482debd6ce642b7012c7a
ViniciusPosssatto <101053966+ViniciusPosssatto@users.noreply.github.com>
2022-07-06 15:52:57 -0300

    Update class RetornaInfos
    Adicionado método que retorna as infos por tipo de veiculo

--- app/entity/retorna_infos/__init__.py
+++ app/entity/retorna_infos/__init__.py
@@ -1,4 +1,5 @@
 from data.infos import carros, camionetes, motosTriciclos, historico_vendas
+from entity.class_veiculo import tipos_veiculos
 
 
 class RetornaInfos:
@@ -20,3 +21,24 @@ class RetornaInfos:
         for i in camionetes:
             print(f"Camionete modelo {i['modelo']}, fabricado em {i['data-fabricação']}, com valor base de "
                   f"R$ {i['valor']}.")
+
+    @staticmethod
+    def retorna_veiculos_por_tipo(tipo):
+        if tipo not in tipos_veiculos:
+            print('Opção de veículo inválida.')
+        for item in tipos_veiculos.get(tipo):
+            if tipo == 'carro':
+                print(f"O modelo é {item['modelo']}, possui {item['portas']} portas e {item['potencia']} "
+                      f"cavalos de potência. A placa é {item['placa'].upper()} e o valor: R$ {item['valor']}. "
+                      f"Foi fabricado em {item['data-fabricação']} na cor {item['cor']}. O número do chassi é "
+                      f"{item['chassi']}.")
+            elif tipo == 'moto':
+                print(f"O modelo é {item['modelo']}, tem {item['potencia']} cavalos de potência. A placa é "
+                      f"{item['placa'].upper()} e o valor: R$ {item['valor']}. O número do chassi é "
+                      f"{item['chassi']}. Foi fabricado em {item['data-fabricação']} na cor {item['cor']}.")
+            elif tipo == 'camionete':
+                print(f"O modelo é {item['modelo']}, possui {item['portas']} portas e {item['potencia']} "
+                      f"cavalos de potência. A placa é {item['placa'].upper()} e o valor: R$ {item['valor']}. "
+                      f"Foi fabricado em {item['data-fabricação']} na cor {item['cor']}. O tamanho da caçamba é de "
+                      f"{item['cacamba']} litros e a numeração do chassi é {item['chassi']}.")
+    
